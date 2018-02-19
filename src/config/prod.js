94f472987bfab12b3cdda7b9bf747e1d81a927d0
fmussap <fabiomussap@yahoo.com>
2018-02-19 10:21:14 +0000

    heroku

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,6 +1,5 @@
 'use strict'
 console.log('prod.js GIPHY_API_KEY', process.env.GIPHY_API_KEY)
-console.log('prod.js GIPHY_PROD_KEY', process.env.GIPHY_PROD_KEY)
 module.exports = {
-  giphyKey: process.env.GIPHY_PROD_KEY
+  giphyKey: process.env.GIPHY_API_KEY
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-const { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId } = require('./src/config/keys')
+const { giphyKey } = require('./src/config/keys')
 
 module.exports = (env) => {
   const isProduction = env === 'production'
@@ -43,12 +43,7 @@ module.exports = (env) => {
     plugins: [
       CSSExtract,
       new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(apiKey),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(authDomain),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(databaseURL),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(projectId),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(storageBucket),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(messagingSenderId)
+        'process.env.GIPHY_API_KEY': JSON.stringify(giphyKey)
       })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
