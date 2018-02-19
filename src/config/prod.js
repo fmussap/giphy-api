f3d3ccc79ba829de5412f5a21596d40604707ed1
fmussap <fabiomussap@yahoo.com>
2018-02-19 10:10:36 +0000

    heroku

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,5 +1,6 @@
 'use strict'
 console.log('prod.js GIPHY_API_KEY', process.env.GIPHY_API_KEY)
+console.log('prod.js GIPHY_PROD_KEY', process.env.GIPHY_PROD_KEY)
 module.exports = {
-  giphyKey: process.env.GIPHY_API_KEY
+  giphyKey: process.env.GIPHY_PROD_KEY
 }
